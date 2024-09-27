b509838bd57294e55234ed3e9fbefbf86ddbad4b
Marcin Parafiniuk <marcin.parafiniuk@codilime.com>
2024-09-27 22:41:16 +0200

    basic read works

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
+#![feature(unix_socket_peek)]
 
+use std::{
+    io::{self, Read},
+    os::unix::net::UnixStream,
+    time::Duration,
+};
 pub mod marsync;
 
 async fn async_main() {
-    let s = marsync::CreateSocket::new(String::from("siemka")).await;
-    match s {
-        Ok(_s) => {
-            println!("socket ok")
+    let s = marsync::create_socket(String::from("/tmp/test.sock")).await;
+    loop {
+        match s {
+            Ok(ref s) => {
+                println!("got socket");
+                let mut buf = [0; 32];
+                let n = s.read(&mut buf).await.unwrap();
+                println!("n {}", n);
+                println!("data {:#?}", buf);
+            }
+            Err(ref e) => println!("socket err {}", e),
         }
-        Err(e) => println!("socket err {}", e),
     }
 }
 
--- src/marsync.rs
+++ src/marsync.rs
@@ -1,14 +1,18 @@
 use once_cell::sync::Lazy;
 use std::borrow::BorrowMut;
-use std::cell::Cell;
+use std::cell::{Cell, RefCell};
 use std::collections::VecDeque;
 use std::error::Error;
+use std::ffi::FromBytesUntilNulError;
 use std::future::Future;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
+use std::mem::MaybeUninit;
+use std::os::fd::AsRawFd;
 use std::os::unix::net::UnixStream;
 use std::pin::Pin;
 use std::sync::{mpsc, Arc, LazyLock, Mutex};
 use std::task::{Context, Poll, Waker};
+use std::time::Duration;
 use thiserror::Error;
 
 use futures::{
@@ -17,7 +21,7 @@ use futures::{
 };
 
 pub struct Socket {
-    s: UnixStream,
+    s: Arc<Mutex<UnixStream>>,
 }
 
 enum SocketTask {
@@ -25,6 +29,11 @@ enum SocketTask {
         String,
         Arc<Mutex<Option<Result<Socket, String>>>>,
         Arc<Mutex<Option<Waker>>>,
+    ),
+    Read(
+        Arc<Mutex<UnixStream>>,
+        Arc<Mutex<Option<Waker>>>,
+        Arc<Mutex<Result<usize, io::Error>>>,
     ),
 }
 
@@ -82,7 +91,7 @@ impl Future for CreateSocket {
 }
 
 impl CreateSocket {
-    pub fn new(path: String) -> Self {
+    fn new(path: String) -> Self {
         let ret = Arc::new(Mutex::new(None));
         let waker = Arc::new(Mutex::new(None));
         let marsync = CONTEXT.lock().unwrap();
@@ -98,6 +107,64 @@ impl CreateSocket {
             ret_socket: ret,
             waker,
         }
+    }
+}
+
+impl Socket {
+    pub fn read<'a, const T: usize>(&self, buf: &'a mut [u8; T]) -> ReadSocket<'a, T> {
+        ReadSocket {
+            ret_data: buf,
+            waker: Arc::new(Mutex::new(None)),
+            s: self.s.clone(),
+            socket_op_status: Arc::new(Mutex::new(Ok(0))),
+        }
+    }
+}
+
+pub struct ReadSocket<'a, const T: usize> {
+    ret_data: &'a mut [u8; T],
+    waker: Arc<Mutex<Option<Waker>>>,
+    s: Arc<Mutex<UnixStream>>,
+    socket_op_status: Arc<Mutex<Result<usize, io::Error>>>,
+}
+
+impl<'a, const T: usize> Future for ReadSocket<'a, T> {
+    type Output = io::Result<usize>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        println!("poll");
+        *self.waker.lock().unwrap() = Some(cx.waker().clone());
+        let other_socket_op_status = self.socket_op_status.clone();
+        let mut socket_result = other_socket_op_status.lock().unwrap();
+        if socket_result.is_err() {
+            let e = socket_result.as_mut().err().unwrap();
+            return Poll::Ready(Err(std::io::Error::new(e.kind(), e.to_string())));
+        }
+        let other_s = self.s.clone();
+        let ret = match other_s.lock().unwrap().read(self.ret_data) {
+            Ok(bytes) => {
+                println!("ok {}", bytes);
+                Poll::Ready(Ok(bytes))
+            }
+            Err(err) => {
+                println!("err {:#?}", err);
+                if err.kind() == std::io::ErrorKind::WouldBlock {
+                    let marsync = CONTEXT.lock().unwrap();
+                    marsync
+                        .socket_tx
+                        .send(Arc::new(SocketTask::Read(
+                            other_s.clone(),
+                            self.waker.clone(),
+                            self.socket_op_status.clone(),
+                        )))
+                        .unwrap();
+                    Poll::Pending
+                } else {
+                    Poll::Ready(Err(err))
+                }
+            }
+        };
+        ret
     }
 }
 
@@ -121,23 +188,67 @@ pub fn run() {
     t2.join().unwrap();
 }
 
+fn socket_thread_create(
+    path: &String,
+    task_ret: &Arc<std::sync::Mutex<Option<Result<Socket, String>>>>,
+    waker: &Arc<std::sync::Mutex<Option<Waker>>>,
+) {
+    let res = UnixStream::connect(path);
+    let mut t = task_ret.lock().unwrap();
+    match res {
+        Ok(s) => {
+            s.set_nonblocking(false).unwrap();
+            s.set_read_timeout(Some(std::time::Duration::from_nanos(1)))
+                .unwrap();
+            *t = Some(Ok(Socket {
+                s: Arc::new(Mutex::new(s)),
+            }));
+        }
+        Err(e) => *t = Some(Err(e.to_string())),
+    }
+    if let Some(w) = waker.lock().unwrap().take() {
+        w.wake();
+    }
+}
+
+fn socket_thread_read(
+    s_arc: &Arc<Mutex<UnixStream>>,
+    waker: &Arc<Mutex<Option<Waker>>>,
+    res: &Arc<Mutex<Result<usize, io::Error>>>,
+) {
+    let s = s_arc.lock().unwrap();
+    let mut r = res.lock().unwrap();
+    println!("waiting for data");
+    match wait_for_data(&s) {
+        Ok(true) => {
+            if let Some(w) = waker.lock().unwrap().take() {
+                w.wake();
+            }
+        }
+        Ok(false) => {
+            *r = Err(std::io::Error::new(io::ErrorKind::TimedOut, ""));
+            if let Some(w) = waker.lock().unwrap().take() {
+                w.wake();
+            }
+        }
+        Err(e) => {
+            *r = Err(e);
+            if let Some(w) = waker.lock().unwrap().take() {
+                w.wake();
+            }
+        }
+    }
+    println!("data arrived");
+}
+
 fn socket_thread(socket_rx: mpsc::Receiver<Arc<SocketTask>>) {
     loop {
         let socket_task = socket_rx.recv().unwrap();
         match socket_task.as_ref() {
             SocketTask::Create(path, task_ret, waker) => {
-                let res = UnixStream::connect(path);
-                let mut t = task_ret.lock().unwrap();
-                match res {
-                    Ok(s) => {
-                        *t = Some(Ok(Socket { s: s }));
-                    }
-                    Err(e) => *t = Some(Err(e.to_string())),
-                }
-                if let Some(w) = waker.lock().unwrap().take() {
-                    w.wake();
-                }
+                socket_thread_create(path, task_ret, waker)
             }
+            SocketTask::Read(s_arc, waker, res) => socket_thread_read(s_arc, waker, res),
         }
     }
 }
@@ -153,5 +264,32 @@ fn executor_thread(task_rx: mpsc::Receiver<Arc<Task>>) {
                 *future_slot = Some(f);
             }
         }
+    }
+}
+
+pub fn create_socket(path: String) -> CreateSocket {
+    CreateSocket::new(path)
+}
+
+fn wait_for_data(stream: &UnixStream) -> io::Result<bool> {
+    let fd = stream.as_raw_fd();
+    let mut read_fds: libc::fd_set = unsafe { MaybeUninit::zeroed().assume_init() };
+    unsafe { libc::FD_ZERO(&mut read_fds) };
+    unsafe { libc::FD_SET(fd, &mut read_fds) };
+
+    let result = unsafe {
+        libc::select(
+            fd + 1,
+            &mut read_fds,
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
+        )
+    };
+
+    match result {
+        -1 => Err(io::Error::last_os_error()),
+        0 => Ok(false), // Timeout
+        _ => Ok(unsafe { libc::FD_ISSET(fd, &read_fds) }),
     }
 }
